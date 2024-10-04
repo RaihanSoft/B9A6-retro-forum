b605fcfe8018c8b466265bffbaa3a99fa570fb57
Raihan <aboraihan14@gmail.com>
2024-10-04 15:38:51 +0600

    latest section added

--- script.js
+++ script.js
@@ -9,14 +9,12 @@ const loadAllPosts = async (category) => {
 
 }
 
-
-
 const displayAllPosts = (posts) => {
 
     const postContainer = document.getElementById("post-container")
     postContainer.innerHTML = ""
     posts.forEach(post => {
-        console.log(post)
+    
 
 
 
@@ -120,3 +118,60 @@ handleSearchByCategory = () => {
     loadAllPosts(searchText)
 
 }
+
+
+// ! latest post section 
+
+const latesPost= async ()=> {
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
+    const data = await res.json()
+    displayPost(data)
+
+}
+
+const displayPost=(data)=>{
+    const latestPostContainer = document.getElementById('latest-post-container')
+   data.forEach(post => {
+
+    const div = document.createElement("div")
+    div.innerHTML = 
+    
+    `
+            <div class="card lg:w-96 pb-5 bg-base-100 shadow-2xl">
+          <figure class="lg:px-6 px-4 pt-4 lg:pt-8">
+            <img src=${post.cover_image} alt="Shoes" class="rounded-xl" />
+          </figure>
+          <div class="p-5 lg:p-10 space-y-4 lg:space-y-5">
+            <p class="opacity-50 text-start">
+            <i class="fa-solid fa-calendar-days me-2"> ${post.author.posted_date? post.author.posted_date : "No Publish Date" }</i>
+            
+            
+       
+            </p>
+            <h2 class="card-title text-start">${post.title}</h2>
+            <p class="text-start">
+             ${post.description}
+            </p>
+            <div class="card-actions flex gap-5 items-center">
+              <div class="avatar">
+                <div class="lg:w-12 w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img src="${post.profile_image}" />
+                </div>
+              </div>
+              <div>
+                <h3 class="text-start font-extrabold">${post.author.name}</h3>
+                <p class="text-start opacity-60">${post.author.designation? post.author.designation : "Unknown"}</p>
+              </div>
+            </div>
+    
+    `
+
+    latestPostContainer.appendChild(div)
+
+
+
+   })
+}
+
+
+latesPost()
